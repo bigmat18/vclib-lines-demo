501db2b7d648e3727011ae5898621f2e558ca30f
Matteo Giuntoni <59317264+bigmat18@users.noreply.github.com>
2025-02-09 23:43:20 +0100

    Adding settings handler

--- include/demo_application.h
+++ include/demo_application.h
@@ -3,9 +3,9 @@
 #include <vclib/render/canvas.h>
 #include <vclib/render/drawers/viewer_drawer.h>
 #include <vclib/render/render_app.h>
-#include <imgui.h>
 
-#include <vclib/bgfx/drawable/lines/drawable_lines.h>
+#include "lines_objects_handler.h"
+#include "lines_settings_handler.h"
 #include "render_utils.h"
 
 #ifndef DEMO_APPLICATION_H
@@ -15,8 +15,8 @@ template <typename DerivedRenderApp>
 class DemoApplication : public vcl::imgui::ImGuiDrawer<DerivedRenderApp>
 {
     using ParentDrawer = vcl::imgui::ImGuiDrawer<DerivedRenderApp>;
-    
     std::vector<std::unique_ptr<vcl::DrawableObject>> mObjects;
+    int mIndexSelected = -1;
 
 public:
     using ParentDrawer::ParentDrawer;
@@ -47,43 +47,16 @@ public:
         ParentDrawer::onDraw(viewId);
 
         if (!ParentDrawer::isWindowMinimized()) {
+            int indexSelected = LinesObjectsHandler::ImGuiRender(mObjects);
+            if(indexSelected != mIndexSelected)
+                mIndexSelected = indexSelected;
 
-            ImVec2 screenSize = ImGui::GetIO().DisplaySize;
+            if(mIndexSelected != -1)
+                LinesSettingsHandler::ImGuiRender(mObjects[mIndexSelected]);
 
-            ImGui::SetNextWindowSize(ImVec2(screenSize.x * 0.25f, screenSize.y), ImGuiCond_Always);
-            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
-            
-            ImGui::Begin("Scene elements", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-
-            for(uint i = 0; i < mObjects.size(); ++i) {
-                if(mObjects[i]->isVisible())
-                    mObjects[i]->draw(viewId);
-
-                float blockHeight = 70.0f;
-                
-                ImVec2 min = ImGui::GetCursorScreenPos();
-                ImVec2 max = ImVec2(min.x + ImGui::GetContentRegionAvail().x + 5, min.y + blockHeight + 5);
-
-                ImDrawList* draw_list = ImGui::GetWindowDrawList();
-                draw_list->AddRectFilled(min, max, IM_COL32(34, 55, 83, 128));
-
-                bool visible = mObjects[i]->isVisible();
-                ImGui::SetCursorScreenPos(ImVec2(min.x + 5, min.y + 5));
-                if (ImGui::Selectable((mObjects[i]->name() + std::to_string(i)).c_str(), &visible, ImGuiSelectableFlags_SpanAllColumns, ImVec2(0, blockHeight))) {
-                    mObjects[i]->setVisibility(!mObjects[i]->isVisible());
-                }
-
-                ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorPosX(), ImGui::GetCursorPosY() - (blockHeight * 0.8)));
-                ImGui::Separator();
-                ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
-                ImGui::TextWrapped("%s", mObjects[i]->info().c_str());
-                ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 30);
-
-            }
-
-            ImGui::End();
+            if(mIndexSelected != -1 && mObjects[mIndexSelected]->isVisible())
+                mObjects[mIndexSelected]->draw(viewId);
         }
-
     }
 };
 #endif
---
+++ include/lines_cube_object.h
@@ -0,0 +1,9 @@
+#ifndef LINES_CUBE_OBJECT
+#define LINES_CUBE_OBJECT
+
+class LinesCubeObject 
+{
+
+};
+
+#endif
