9e73518ee42f9adfbb1a0dc354098fc6dcb44e54
Matteo Giuntoni <59317264+bigmat18@users.noreply.github.com>
2025-02-09 17:39:12 +0100

    init side menu

--- include/demo_application.h
+++ include/demo_application.h
@@ -6,6 +6,7 @@
 #include <imgui.h>
 
 #include <vclib/bgfx/drawable/lines/drawable_lines.h>
+#include "render_utils.h"
 
 #ifndef DEMO_APPLICATION_H
 #define DEMO_APPLICATION_H
@@ -22,14 +23,23 @@ public:
 
     DemoApplication(uint, uint) 
     {
-        std::vector<vcl::lines::LinesVertex> points = {
-            vcl::lines::LinesVertex(0, 0.5, 0, vcl::lines::LinesVertex::COLOR(1, 0, 0, 1)),
-            vcl::lines::LinesVertex(0.5, 0, 0, vcl::lines::LinesVertex::COLOR(1, 0, 0, 1)),
-        };
-        mObjects.push_back(std::make_unique<vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>>(
-            vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>(points)
-        ));
 
+        std::vector<vcl::lines::LinesVertex> points;
+
+        generatePointsInCube(points, 3, 1000);
+        vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines> line1 = vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>(points);
+        line1.setName("Lines in cube 1");
+        line1.setInfo("Set of lines in a cube of size " + std::to_string(5) + " with " + std::to_string(points.size() / 2) + " segments inside");
+        line1.setVisibility(false);
+
+        generatePointsInCube(points, 3, 1000);
+        vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines> line2 = vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>(points);
+        line2.setName("Lines in cube 2");
+        line2.setInfo("Set of lines in a cube of size " + std::to_string(5) + " with " + std::to_string(points.size() / 2) + " segments inside");
+        line2.setVisibility(false);
+
+        mObjects.push_back(std::make_unique<vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>>(line1));
+        mObjects.push_back(std::make_unique<vcl::lines::DrawableLines<vcl::lines::CPUGeneratedLines>>(line2));
     };
 
     virtual void onDraw(vcl::uint viewId) override
@@ -37,10 +47,40 @@ public:
         ParentDrawer::onDraw(viewId);
 
         if (!ParentDrawer::isWindowMinimized()) {
-            for(auto& obj : mObjects)
-                obj->draw(viewId);
-            ImGui::Begin("Test");
-            ImGui::Text("Ciao");
+
+            ImVec2 screenSize = ImGui::GetIO().DisplaySize;
+
+            ImGui::SetNextWindowSize(ImVec2(screenSize.x * 0.25f, screenSize.y), ImGuiCond_Always);
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+            
+            ImGui::Begin("Scene elements", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+            for(uint i = 0; i < mObjects.size(); ++i) {
+                if(mObjects[i]->isVisible())
+                    mObjects[i]->draw(viewId);
+
+                float blockHeight = 70.0f;
+                
+                ImVec2 min = ImGui::GetCursorScreenPos();
+                ImVec2 max = ImVec2(min.x + ImGui::GetContentRegionAvail().x + 5, min.y + blockHeight + 5);
+
+                ImDrawList* draw_list = ImGui::GetWindowDrawList();
+                draw_list->AddRectFilled(min, max, IM_COL32(34, 55, 83, 128));
+
+                bool visible = mObjects[i]->isVisible();
+                ImGui::SetCursorScreenPos(ImVec2(min.x + 5, min.y + 5));
+                if (ImGui::Selectable((mObjects[i]->name() + std::to_string(i)).c_str(), &visible, ImGuiSelectableFlags_SpanAllColumns, ImVec2(0, blockHeight))) {
+                    mObjects[i]->setVisibility(!mObjects[i]->isVisible());
+                }
+
+                ImGui::SetCursorScreenPos(ImVec2(ImGui::GetCursorPosX(), ImGui::GetCursorPosY() - (blockHeight * 0.8)));
+                ImGui::Separator();
+                ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 5);
+                ImGui::TextWrapped("%s", mObjects[i]->info().c_str());
+                ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 30);
+
+            }
+
             ImGui::End();
         }
 
--- include/render_utils.h
+++ include/render_utils.h
@@ -7,6 +7,9 @@
 #include <vclib/meshes.h>
 #include <vclib/render/drawable/drawable_mesh.h>
 
+#include <random>
+#include <cmath>
+
 template<vcl::MeshConcept MeshType>
 inline vcl::DrawableMesh<MeshType> getMesh(const std::string& filename = "bimba.obj")
 {
@@ -24,6 +27,25 @@ inline vcl::DrawableMesh<MeshType> getMesh(const std::string& filename = "bimba.
     drawable.setRenderSettings(settings);
 
     return drawable;
+}
+
+void generatePointsInCube(std::vector<vcl::lines::LinesVertex> &points, float size, float n) {
+    points.clear();
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> color(0, 1);
+    std::uniform_real_distribution<> coord(size, -size);
+
+    for (int i = 0; i < n; i++) {
+        points.push_back(
+            vcl::lines::LinesVertex(coord(gen), coord(gen), coord(gen), 
+            vcl::lines::LinesVertex::COLOR(color(gen), color(gen), color(gen), 1.0))
+        );
+        points.push_back(
+            vcl::lines::LinesVertex(coord(gen), coord(gen), coord(gen), 
+            vcl::lines::LinesVertex::COLOR(color(gen), color(gen), color(gen), 1.0))
+        );
+    }
 }
 
 #endif
